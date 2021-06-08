f2623a1e775619e81ee35944f23f1dd7b055bb8a
ahmedMshaban <ahmed.ms.elias@gmail.com>
2021-06-08 08:04:24 +0300

    Get quotes via quotable api

--- src/App.js
+++ src/App.js
@@ -1,70 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import QuoteText from "./components/QuoteText";
 import QuoteAuthor from "./components/QuoteAuthor";
 import NewQuote from "./components/NewQuote";
 import ShareQuote from "./components/ShareQuote";
 
-const Quotes = [
-  {
-    text: "You take your life in your own hands, and what happens? A terrible thing, no one to blame.",
-    author: "Erica Jong",
-  },
-  {
-    text: "You miss 100% of the shots you don't take.",
-    author: "Wayne Gretzky",
-  },
-  {
-    text: "Weeks of programming can save you hours of planning.",
-    author: "Unknown",
-  },
-  {
-    text: "First learn computer science and all the theory. Next develop a programming style. Then forget all that and just hack.",
-    author: "George Carrette",
-  },
-  {
-    text: "The harder the conflict, the more glorious the triumph.",
-    author: "Thomas Paine",
-  },
-  {
-    text: "The question of whether computers can think is like the question of whether submarines can swim.",
-    author: "E.W. Dijkstra",
-  },
-  {
-    text: "What matters most is how well you walk through the fire.",
-    author: "Charles Bukowski",
-  },
-  {
-    text: "You will do foolish things, but do them with enthusiasm.",
-    author: "Colette",
-  },
-  {
-    text: "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.",
-    author: "Eagleson's Law",
-  },
-  {
-    text: "The scariest moment is always just before you start.",
-    author: "Stephen King",
-  },
-  {
-    text: "Don't let yesterday take up too much of today.",
-    author: "Will Rogers",
-  },
-];
-
 function App() {
-  const [currentQuote, setCurrentQuote] = useState(Math.floor(Math.random() * Quotes.length));
-  const generateRandomQuote = () => {
-    setCurrentQuote(Math.floor(Math.random() * Quotes.length)); // returns a random integer from 0 to Quotes length
-  };
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const [newQuote, setNewQuote] = useState(false);
 
-  return (
-    <div id="quote-box">
-      <QuoteText text={Quotes[currentQuote].text} />
-      <QuoteAuthor author={Quotes[currentQuote].author} />
-      <NewQuote onClickHandler={generateRandomQuote} />
-      <ShareQuote text={Quotes[currentQuote].text}/>
-    </div>
-  );
+  const generateRandomQuote = () => {
+    setNewQuote(!newQuote);
+  }
+
+  useEffect(() => {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, [newQuote]);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div id="quote-box">
+        <QuoteText text={items.content} />
+        <QuoteAuthor author={items.author} />
+        <NewQuote onClickHandler={generateRandomQuote} />
+        <ShareQuote text={items.content} />
+      </div>
+    );
+  }
 }
 
 export default App;
